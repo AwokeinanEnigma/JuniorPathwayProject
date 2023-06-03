745baa495b2c75398c97e69d42fced5f0241f780
Enigma <49351666+AwokeinanEnigma@users.noreply.github.com>
2023-06-03 09:23:39 -0400

    add polymorphism

---
+++ Assets/GenericBox.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+namespace DefaultNamespace
+{
+    // demonstrates inheritance
+    public class GenericBox : Box
+    {
+        // demonstrates polymorphism
+        public override void OnPickedUp()
+        {
+            Debug.Log("Hello, world!");
+        }
+
+        public override void OnDropped()
+        {
+            Debug.Log("Goodbye, world!");
+        }
+
+        public override void WhilePickedup()
+        {
+            base.WhilePickedup();
+            Debug.Log("I'm being held!");
+        }
+    }
+}
