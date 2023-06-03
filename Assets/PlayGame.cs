1aa5d9354b43e463691ff9e94616c020fd890aff
Enigma <49351666+AwokeinanEnigma@users.noreply.github.com>
2023-06-03 13:55:44 -0400

    add title screen

---
+++ Assets/PlayGame.cs
@@ -0,0 +1,23 @@
+﻿using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+namespace DefaultNamespace
+{
+    public class PlayGame : MonoBehaviour
+    {
+        public void QuitGame()
+        {
+            Application.Quit();
+        }
+        
+        public TMP_InputField NameInput;
+        
+        public void StartGame()
+        {
+            SceneManager.LoadScene("GameScene");
+            PlayerPrefs.SetString("Name", NameInput.text);
+        }
+    }
+}
